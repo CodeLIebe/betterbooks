b0fd5e1fb9c3f1b16682b7967c3448ff7be7b963
CodeLIebe <efughe@gmail.com>
2018-11-29 20:02:47 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+
+// Set the browser that you want to support
+const AUTOPREFIXER_BROWSERS = [
+  'chrome >= 34'
+];
+
+// Gulp task to minify CSS files
+gulp.task('css', function () {
+  return gulp.src('./sass/*.scss')
+    // Compile SASS files
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    // Auto-prefix css styles for cross browser compatibility
+    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    //Output - save as .css
+    .pipe(gulp.dest('./prod/style/'))
+});
+
+
+
+// Gulp task to watch all files
+gulp.task('default', function () {
+
+  //watch task to update files automatically
+  gulp.watch('./sass/*.scss',['css']);
+});
