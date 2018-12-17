9b25b14766e5fcae7f85eda4a041805a5c9d1945
Amer El-Ankah <amer.el-ankah@gastro-cashbox.com>
2018-12-17 20:35:56 +0100

    Added switch to change dark to light theme

--- prod/js/betterbooks.js
+++ prod/js/betterbooks.js
@@ -1,3 +1,4 @@
+var darkThemeSelected = false;
 
 //make dropup parent clickable
 function clickDropdown() {
@@ -18,8 +19,56 @@ $(document).ready(function () {
 
 
 //close quiz when clicked in overlay
-$("#modal-wrapper").on('click', function () { 
+$("#modal-wrapper").on('click', function () {
    hideQuiz();
 }).children().on('click', function (e) {
     e.stopPropagation();
+});
+
+function toggle() {
+  darkThemeSelected = !darkThemeSelected
+  if(darkThemeSelected)
+  {
+      alert("Dark");
+      // Show dark theme
+      document.getElementById("theme").href="style/betterbooks_dark.css";
+  }
+  else
+  {
+      alert("Light");
+      // Show dark theme
+      document.getElementById("theme").href="style/betterbooks_light.css";
+  }
+}
+$("#theme-switch123").on('change', function() {
+    if ($(this).is(':checked')) {
+        darkThemeSelected = $(this).is(':checked');
+        document.getElementById("theme").href="style/betterbooks_light.css";
+        //alert(darkThemeSelected);// To verify
+    }
+    else {
+       darkThemeSelected = $(this).is(':checked');
+       document.getElementById("theme").href="style/betterbooks_dark.css";
+       //alert(darkThemeSelected);// To verify
+    }
+});
+
+
+$('#theme-switch123').click(function(){
+$('.tab').hide();
+darkThemeSelected = !darkThemeSelected
+darkThemeSelected = !darkThemeSelected
+if(darkThemeSelected)
+{
+    alert("Dark");
+    // Show dark theme
+    document.getElementById("theme").href="style/betterbooks_dark.css";
+}
+else
+{
+    alert("Light");
+    // Show dark theme
+    document.getElementById("theme").href="style/betterbooks_light.css";
+}
+//alert($('#myonoffswitch').attr('checked'));
 });
